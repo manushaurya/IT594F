b72462eb0359f967638ba6401cfdb40fe076a071
Manu Shaurya <shauryamanu50@gmail.com>
2016-12-20 00:18:38 +0530

    Add files via upload

---
+++ palindrome.cpp
@@ -0,0 +1,28 @@
+/*Using call by value */
+#include <iostream>
+using namespace std;
+
+void palindrome(int);
+void palindrome(int n)
+{
+    int i=0,c;
+    c=n;
+    while(n>0)
+    {
+        i=i*10+n%10;
+        n=n/10;
+    }
+    if(i==c)
+    cout<<"No. is palindrome !!!"<<endl;
+    else
+    cout<<"No. is not palindrome !!!"<<endl;
+}
+
+int main()
+{
+    int n;
+    cout<<"--------------Palindrome Test--------------------"<<endl<<"Enter a number: "<<endl;
+    cin>>n;
+    palindrome(n);
+    return 0;
+}
