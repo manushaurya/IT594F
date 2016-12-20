96e1fba19916b7893ed69ce7ec458b807a7c4072
Manu Shaurya <shauryamanu50@gmail.com>
2016-12-20 17:06:38 +0530

    Update exception without function.cpp

--- exception without function.cpp
+++ exception without function.cpp
@@ -1,1 +1,27 @@
-
+#include <iostream>
+using namespace std;
+int main()
+{
+  float num,den;
+  cout<<"Enter the numerator: ";
+  cin>>num;
+  cout<<"Enter the denominator: ";
+  cin>>den;
+  float result=num/den;
+  if(den!=0)
+  {
+    cout<<"Result: "<<result<<endl;
+  }
+  else
+  {
+    try
+    {
+      throw result;
+    }
+    catch(float result)
+    {
+      cout<<"Can't divide by zero!!"<<endl;
+    }
+  }
+  return 0;
+}
